13481a5f8e4ec0e430843c265ab495529d90d7f6
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-06 12:54:20 +0600

    added task4

---
+++ task4.js
@@ -0,0 +1,14 @@
+// Task-4
+// Count the number of properties.
+
+// Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const keys = Object.keys(student);
+console.log(keys);
+console.log(keys.length);
